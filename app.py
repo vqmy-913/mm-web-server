e8ad6d1313aef0388866fd8244c70bd71bc6c12c
My Nguyen <81615875+vqmy-913@users.noreply.github.com>
2024-03-10 20:20:40 +1100

    Linter

--- app.py
+++ app.py
@@ -1,34 +1,36 @@
-from flask import Flask
 from pymongo import MongoClient
 from flask import Flask, render_template, request, url_for, redirect
-from pymongo import MongoClient
 from bson.objectid import ObjectId
 
 # change to another port (8000) with: flask run --port 8000
 app = Flask(__name__)
 
-client = MongoClient('localhost', 27017)
-
+# set up client in MongoDB
+client = MongoClient("localhost", 27017)
 db = client.flask_db
 todos = db.todos
 
-@app.route('/', methods=('GET', 'POST'))
+
+# create index page from template
+@app.route("/", methods=("GET", "POST"))
 def index():
-    if request.method=='POST':
-        content = request.form['content']
-        degree = request.form['degree']
-        todos.insert_one({'content': content, 'degree': degree})
-        return redirect(url_for('index'))
-    
+    if request.method == "POST":
+        content = request.form["content"]
+        degree = request.form["degree"]
+        todos.insert_one({"content": content, "degree": degree})
+        return redirect(url_for("index"))
+
     all_todos = todos.find()
-    return render_template('index.html', todos=all_todos)
+    return render_template("index.html", todos=all_todos)
+
 
 # accepts POST requests
 # since web browsers default to GET requests, user clicks button > POST request to delete a todo
-@app.post('/<id>/delete/')
+# delete button
+@app.post("/<id>/delete/")
 def delete(id):
     todos.delete_one({"_id": ObjectId(id)})
-    return redirect(url_for('index'))
+    return redirect(url_for("index"))
 
 
 # edit button
