9a89263ea8da8fdd520afeb91471817755a19049
My Nguyen <81615875+vqmy-913@users.noreply.github.com>
2024-03-11 23:56:50 +1100

    Convert into restaurant rating list

--- app.py
+++ app.py
@@ -8,28 +8,28 @@ app = Flask(__name__)
 # set up client in MongoDB
 client = MongoClient("localhost", 27017)
 db = client.flask_db
-todos = db.todos
+ratings = db.ratings
 
 
 # create index page from template
 @app.route("/", methods=("GET", "POST"))
 def index():
     if request.method == "POST":
-        content = request.form["content"]
-        degree = request.form["degree"]
-        todos.insert_one({"content": content, "degree": degree})
+        restaurant = request.form["restaurant"]
+        rating = request.form["rating"]
+        ratings.insert_one({"restaurant": restaurant, "rating": rating})
         return redirect(url_for("index"))
 
-    all_todos = todos.find()
-    return render_template("index.html", todos=all_todos)
+    all_ratings = ratings.find()
+    return render_template("index.html", ratings=all_ratings)
 
 
 # accepts POST requests
-# since web browsers default to GET requests, user clicks button > POST request to delete a todo
+# since web browsers default to GET requests, user clicks button > POST request to delete a rating
 # delete button
 @app.post("/<id>/delete/")
 def delete(id):
-    todos.delete_one({"_id": ObjectId(id)})
+    ratings.delete_one({"_id": ObjectId(id)})
     return redirect(url_for("index"))
 
 
@@ -37,12 +37,14 @@ def delete(id):
 @app.post("/<id>/update/")
 def update(id):
     if request.method == "POST":
-        new_content = request.form["content"]
-        new_degree = request.form["degree"]
-        todos.update_one(
+        new_content = request.form["restaurant"]
+        new_rating = request.form["rating"]
+        ratings.update_one(
             {"_id": ObjectId(id)},
-            {"$set": {"content": new_content, "degree": new_degree}},
+            {"$set": {"restaurant": new_content, "rating": new_rating}},
         )
         return redirect(url_for("index"))
-    return render_template("index.html", todos=todos.find())
+    return render_template("index.html", ratings=ratings.find())
 
+
+# done button
